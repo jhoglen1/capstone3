05db341cc86b86acee0ad2c666f588c206b150ca
Jeffrey Hoglen <jeffhoglen@gmail.com>
2019-03-19 19:56:51 -0400

    heroku

--- public/workout.js
+++ public/workout.js
@@ -1,16 +1,36 @@
 
 
 
-
-//$("#workouts-page").on("click", ".details", function() {
-  //  hideAllPages();
-
+ 
   $(".log-out").on("click", function(e) {
     e.preventDefault();
     $("#workouts-page").hide();
     $(location).attr("href","./index.html")
   });
+  $("#work").on("click", function(e) {
+    e.preventDefault();
+    $(".daily").show();
+    
   
+  }); 
+  
+  function display(responseJson) {
+   
+    for (let i = 0; i < responseJson.length; i++){
+
+$('.workout-section').append(`<div class="daily">
+     
+<input class="id" type="text" value="${responseJson[i]._id}" >      
+<input class="Day" type="text" value="${responseJson[i].Day}" >
+<input class="Miles" type="text" value="${responseJson[i].Miles}" >
+<input class="HR" type="text" value="${responseJson[i].HR}" >
+<input type="button" class="edit_button1" value="Edit" class="edit">
+<input type="button" class="save_button1" value="Save" class="save">
+<input type="button" value="Delete" class="delete">
+</div>`);      
+}
+} 
+
 
 /* rest */
 /*function getWorkouts() {
@@ -31,6 +51,4 @@
         console.log("error");
       }
     });
-}
-//})
-*/
+}*/
