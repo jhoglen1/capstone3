f1305d268f0b18fdef84426f355736123c0cfce6
Jeffrey Hoglen <jeffhoglen@gmail.com>
2019-03-22 22:54:48 -0400

    README

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 'use strict';
 exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb+srv://jhoglen1:Emerson21!@cluster0-lk6aj.mongodb.net/cycling-pro?retryWrites=true';
-//exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb+srv://jhoglen1:Emerson21!@cluster0-lk6aj.mongodb.net/cycling-pro?retryWrites=true';
 exports.PORT = process.env.PORT || 8080;
 exports.JWT_SECRET = process.env.JWT_SECRET || "jeff" ;
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
--- public/workout.js
+++ public/workout.js
@@ -13,45 +13,23 @@
     error: function(error) {
       console.log("error");
       $(location).attr("href","./index.html")
-    
-      
-    }
+     }
   });
 
- 
   $(".log-out").on("click", function(e) {
     e.preventDefault();
     $(location).attr("href","./index.html")
     localStorage.setItem("authToken","");
   });
 
-
-  
-
-  
-   
-
-   
-  
-  
-
-
-
-  
-  $(".edit-form").on("submit", function(e) {
+ $(".edit-form").on("submit", function(e) {
       e.preventDefault();
   })
-   
-
-      $(".create-workout").on("click", function(e) {
+   $(".create-workout").on("click", function(e) {
         e.preventDefault();
         $(".back").show();
        
-       
-       
-       
-     
-      let workout = {
+        let workout = {
         day: $("#day").val(),
         miles: $("#miles").val(),
         hr: $("#hr").val(),
@@ -72,14 +50,8 @@
     $("#workouts-page").on("click", ".delete", function(e) {
       e.preventDefault();
       let id = $(this).attr("data-id");
-     
-  
-          deleteWorkout(id);
+       deleteWorkout(id);
         }); 
-
-    
-    
-    
     function getWorkouts() {
       console.log("Getting workouts information");
       let authToken = localStorage.getItem("authToken");
@@ -98,34 +70,24 @@
         }
       });
     }
-   
     function displayResults(workoutArray) {
-  
-      console.log(workoutArray);
+     console.log(workoutArray);
      $('#results-list').empty();
       for (let i = 0; i < workoutArray.length; i++){
         let workout = workoutArray[i];
-      
-        $('#results-list').append(
+       $('#results-list').append(
           `<section class="workout-section" data-index="${i}">   </section>
          <ul>
         <li>${workout.day}</li>
          <li>miles: ${workout.miles}</li>
           <li>hr: ${workout.hr}</li>
           <button class="delete" data-id="${workout.id}">Delete</button>
-         
-      
-         </li>
-         </ul>
-    
-         `)
+          </li>
+         </ul> `)
          };
         }
 
-      
-
-
-    function addWorkout(workout){
+        function addWorkout(workout){
       console.log("Adding workout", workout);
       let authToken = localStorage.getItem("authToken");
       $.ajax({
